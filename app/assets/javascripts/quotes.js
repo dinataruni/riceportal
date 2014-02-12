41a00ccf03b18c6b4584802b45e1cb3cb5ede289
Dina Graves <dinataruni@gmail.com>
2014-02-11 22:24:20 -0800

    Tabletools buttons showing

--- app/assets/javascripts/quotes.js
+++ app/assets/javascripts/quotes.js
@@ -1,8 +1,42 @@
 $(document).ready( function () {
   $('#quotes').dataTable( {
-    "sDom": 'T<"clear">lfrtip',
+     "sDom": "<'row-fluid'<'span6'T><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
     "oTableTools": {
-      "sSwfPath": "swf/copy_csv_xls_pdf.swf"
+      "sSwfPath": "swf/copy_csv_xls_pdf.swf",
+      "aButtons": [
+        "copy",
+        "print",
+        {
+          "sExtends":    "collection",
+          "sButtonText": 'Save <span class="caret" />',
+          "aButtons":    [ "csv", "xls", "pdf" ]
+          
+        }
+      ]
     }
   } );
+} );
+
+$.extend( true, $.fn.DataTable.TableTools.classes, {
+  "container": "btn-group",
+  "buttons": {
+    "normal": "btn",
+    "disabled": "btn disabled"
+  },
+  "collection": {
+    "container": "DTTT_dropdown dropdown-menu",
+    "buttons": {
+      "normal": "",
+      "disabled": "disabled"
+    }
+  }
+} );
+
+// Have the collection use a bootstrap compatible dropdown
+$.extend( true, $.fn.DataTable.TableTools.DEFAULTS.oTags, {
+  "collection": {
+    "container": "ul",
+    "button": "li",
+    "liner": "a"
+  }
 } );
